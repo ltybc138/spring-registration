c833b5dbe9f072af6bb82706e453793a285ff99e
Semenov Denis <denis.semenov.prog@gmail.com>
2018-08-12 13:51:14 +0500

    Initial commit

---
+++ src/main/java/com/ltybc/security/springregistration/SpringRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.ltybc.security.springregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRegistrationApplication.class, args);
+	}
+}
